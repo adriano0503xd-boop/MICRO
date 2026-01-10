8a300f7b0b8a267d8b15f72519a343f38ff8ee2f
adriano0503xd-boop <adriano0503xd@gmail.com>
2026-01-10 04:16:24 -0500

    Update AIO_KEY definition in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURACIÓN (¡PON TUS DATOS AQUÍ!)
 // ==========================================
 const AIO_USERNAME = "Fran25"; 
-const AIO_KEY = "aio_rXMK98tltTFyTFISHsSwhRj3eiGE";           
+const AIO_KEY = "aio_rXMK98tltTFyTFISHsSwhRj3eiGE";          
 
 const FEED_TEMP = AIO_USERNAME + "/feeds/temperatura";
 const FEED_HUM = AIO_USERNAME + "/feeds/humedad";
