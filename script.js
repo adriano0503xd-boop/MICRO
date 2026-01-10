437038eaba6dacb0ceda43ac1e17e82284ff3091
adriano0503xd-boop <adriano0503xd@gmail.com>
2026-01-10 03:19:51 -0500

    Update AIO_KEY for Adafruit IO configuration

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURACIÓN ADAFRUIT IO
 // ==========================================
 const AIO_USERNAME = "Fran25";
-const AIO_KEY = "aio_vBKB0906QiSoVEFHt3PVeYb0GBsL";
+const AIO_KEY = "aio_xMPG535tY6PDNKh7XDBf3UPnoWnZ";
 const FEED_TEMP = AIO_USERNAME + "/feeds/temperatura";
 const FEED_HUM = AIO_USERNAME + "/feeds/humedad";
 const FEED_LUM = AIO_USERNAME + "/feeds/luminosidad";
